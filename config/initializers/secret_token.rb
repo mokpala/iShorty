32d73a7b284d5fe439a12ffe23ad6767ea34ba2e
Ikenna Okpala <ikennaokpala@googlemail.com>
2011-10-08 12:50:45 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IShorty::Application.config.secret_token = 'cfe31b4a55288067364b30392f0d4eeed1e62ecb820e6ea55de4246c768272ca477ca06fa9f38c112060eabdccd3c04a92a719fc881e706bc380b9f6c31d50b5'
